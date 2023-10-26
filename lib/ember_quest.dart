13110209fd5fe76f0f36b395fcf4ee6ca68e9d66
miyaji555 <kou.sepak@gmail.com>
2023-10-26 21:39:13 +0900

    create EmberQuest class

---
+++ lib/ember_quest.dart
@@ -0,0 +1,18 @@
+import 'package:flame/game.dart';
+
+class EmberQuestGame extends FlameGame {
+  EmberQuestGame();
+
+  @override
+  Future<void> onLoad() async {
+    await images.loadAll([
+      'block.png',
+      'ember.png',
+      'ground.png',
+      'heart_half.png',
+      'heart.png',
+      'star.png',
+      'water_enemy.png',
+    ]);
+  }
+}
