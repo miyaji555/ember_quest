9538ae67f04930a27f2273f70e112a09b15f4869
miyaji555 <kou.sepak@gmail.com>
2023-10-28 08:42:23 +0900

    delete unnecessary import

--- lib/actors/ember.dart
+++ lib/actors/ember.dart
@@ -1,7 +1,6 @@
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flame/effects.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_template/actors/water_enemy.dart';
 
